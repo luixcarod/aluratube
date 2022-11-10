7006f6d20ea0b5b8e7a7b53512311b6890b09307
Luiz Carlos <luiz-carlos.jales@hotmail.com>
2022-11-10 17:17:16 -0300

    atualizacao da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import config from '../config.json';
 import styled from 'styled-components';
 import { CSSReset } from '../src/components/CSSReset';
@@ -5,6 +6,8 @@ import Menu from '../src/components/Menu';
 import { StyledTimeline } from '../src/components/Timeline';
 
 function HomePage() {
+
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("Shaco");
 
     return (
         <>
@@ -15,13 +18,16 @@ function HomePage() {
                 flex: 1,
                 // backgroundColor: "red",
             }}>
-                <Menu />
+
+                {/* Prop Drilling - que é quando "furamos" as camadas do react para passar
+                propriedades de um componente para seu children */}
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
 
                 <Header />
 
                 {/* Aqui na timeline o que passei: playlists={config.playlists} 
                 é conhecido como props (as propriedades desse componente) */}
-                <Timeline playlists={config.playlists} />
+                <Timeline playlists={config.playlists} favoritos={config.favoritos} searchValue={valorDoFiltro} />
             </div>
         </>
     )
@@ -73,13 +79,35 @@ function Header() {
     )
 }
 
-function Timeline(props) {
+const StyledFavorito = styled.div`
+    img {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+
+    span {
+        font-size: 14px;
+        width: 100%;
+    }
+
+    a {
+        width: 100px;
+        height: 100%;
+        gap: 8px;
+    }
+`;
+
+function Timeline({ searchValue, ...props }) {
 
     //Aqui criamos uma variavel que recebe um array que contém as keys do arrey de playlists
     //nesse caso ele vai dar um retorno com os nomes de cada arrey...
     //Lembrando que o object.kays é uma função de javascript pura, pois o react se escora muito no js
+
     const playlistNames = Object.keys(props.playlists);
     // console.log(`playlistsName: ${playlistNames}`);
+
+    const favoritosNames = Object.keys(props.favoritos);
 
 
     //Statement
@@ -103,18 +131,30 @@ function Timeline(props) {
                     <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
-                                // aqui mapeamos cada video dentro de cada playlist, renderizando os videos
-                                // em sua propria playlist
-                                return (
-                                    <a key={video.url} href={video.url}>
-                                        <img src={video.thumb} />
-                                        <span>
-                                            {video.title}
-                                        </span>
-                                    </a>
-                                )
-                            })}
+                            {videos
+                                .filter((video) => {
+                                    //aqui criamos duas novas variaveis "titleNormalized e searchValueNormalized" que 
+                                    //recebe o titulo da busca
+                                    //e a função toLowerCase() faz com que indiferente do case da fonte, ela exiba
+                                    //a busca. Ex: a == A ou S == s. Não faz diferença entre a fonte maiuscula ou
+                                    //minuscula...
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+
+                                    return titleNormalized.includes(searchValueNormalized);
+                                })
+                                .map((video) => {
+                                    // aqui mapeamos cada video dentro de cada playlist, renderizando os videos
+                                    // em sua propria playlist
+                                    return (
+                                        <a key={video.url} href={video.url}>
+                                            <img src={video.thumb} />
+                                            <span>
+                                                {video.title}
+                                            </span>
+                                        </a>
+                                    )
+                                })}
                         </div>
                     </section>
                     //moral desse código, ele cria um arrey com os nomes das playlists
@@ -124,6 +164,30 @@ function Timeline(props) {
                     //acabda a playlist 01, vem a playlist 02 fazendo a mesma coisa...
                 )
             })}
+
+            {favoritosNames.map(() => {
+                const favorito = props.favoritos[favoritosNames];
+
+                return (
+                    <section key={favoritosNames}>
+                        <h2>{favoritosNames}</h2>
+                        <StyledFavorito>
+                            {favorito.map((favorito) => {
+                                return (
+                                    <a key={favorito.url} href={favorito.url}>
+                                        <img src={favorito.thumb} />
+                                        <span>
+                                            @{favorito.nomecanal}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </StyledFavorito>
+                    </section>
+                )
+            })
+
+            }
         </StyledTimeline>
     )
 }
