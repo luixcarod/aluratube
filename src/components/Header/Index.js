35c0c45dc45d6a8da9ea5d5bda60cdfc852f3eee
Luiz Carlos <luiz-carlos.jales@hotmail.com>
2022-11-13 16:53:04 -0300

    atualizacao final aluratube

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -20,8 +20,8 @@ export default function Header() {
                 </div>
                 <div className="social">
                         <span>
-                            <a href="https://github.com/luixcarod"><AiOutlineGithub /></a>
-                            <a href="https://www.linkedin.com/in/luiz-carlos-rodrigues-a7611679/"><AiFillLinkedin /></a>
+                            <a href="https://github.com/luixcarod" target="_blank"><AiOutlineGithub /></a>
+                            <a href="https://www.linkedin.com/in/luiz-carlos-rodrigues-a7611679/" target="_blank"><AiFillLinkedin /></a>
                         </span>
                     </div>
             </section>
